fb211041d21616f1b9fff4898c45d9d4862ccc47
Guilherme Ribeiro <guilhermeh1609@gmail.com>
2026-09-21 21:39:11 -0300

    feat: atualiza script do banco de dados

--- script.sql
+++ script.sql
@@ -17,7 +17,6 @@ create table usuarios (
     email varchar(100) unique not null,
     cargo varchar(100) not null,
     senha_hash varchar(255) not null,
-    ativo boolean default true,
     time_id int,
     data_criacao datetime default current_timestamp,
     constraint fk_usuario_time foreign key (time_id) references time(id_time) on delete set null
@@ -29,11 +28,11 @@ add constraint fk_time_responsavel foreign key (responsavel_id) references usuar
 create table os(
     os_id int primary key auto_increment,
     os_titulo varchar(100) not null,
-    os_descricao text not null,
-    os_status enum('aberta', 'em andamento', 'concluida') default 'aberta',
+    os_descricao text,
+    os_status enum('aberta', 'em andamento', 'concluida', 'teste', 'bloqueado', 'review') default 'aberta',
     prioridade enum('baixa', 'media', 'alta', 'critica') default 'media',
     os_cliente varchar(100) not null,
-    data_limite date not null,
+    data_limite date,
     id_criador int not null,
     id_time_responsavel int, 
     data_criacao datetime default current_timestamp,
@@ -43,13 +42,36 @@ create table os(
 
 
 create table anexo(
-anexo_id int primary key auto_increment,
-os_id int not null,
-nome_anexo varchar(100) not null,
-anexo_tipo varchar(100) not null,
-anexo_tamanho bigint,
-data_anexo datetime default current_timestamp,
-constraint fk_anexos_os foreign key (os_id) REFERENCES os(os_id) ON DELETE CASCADE
+    anexo_id int primary key auto_increment,
+    os_id int not null,
+    nome_anexo varchar(100) not null,
+    anexo_tipo varchar(100) not null,
+    anexo_tamanho bigint,
+    data_anexo datetime default current_timestamp,
+    constraint fk_anexos_os foreign key (os_id) references os(os_id) on delete cascade);
+
+create table projetos (
+    projeto_id int primary key auto_increment,
+    nome varchar(150) not null,
+    data_assinatura_contrato date,
+    data_implantacao date,
+    data_vencimento_contrato date,
+    responsavel_id int,
+    vendedor_id int,
+    tipo_sistema varchar(100) not null,
+    equipamentos_hardware text,
+    data_criacao datetime default current_timestamp,
+    constraint fk_projeto_responsavel foreign key (responsavel_id) references usuarios(id_usuario) on delete set null,
+    constraint fk_projeto_vendedor foreign key (vendedor_id) references usuarios(id_usuario) on delete set null
 );
 
-
+create table manutencoes (
+    manutencao_id int primary key auto_increment,
+    projeto_id int not null,
+    data_inicio_problema date not null,
+    tipo_manutencao enum('corretiva', 'preventiva', 'evolutiva', 'adaptativa') not null,
+    descricao_situacao text not null,
+    status enum('aberta', 'em andamento', 'concluida', 'cancelada') default 'aberta',
+    data_criacao datetime default current_timestamp,
+    constraint fk_manutencao_projeto foreign key (projeto_id) references projetos(projeto_id) on delete cascade
+);
